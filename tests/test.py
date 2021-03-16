d6649114525b5e756ced420d658d7f4e6ed187c0
Yuji Yamamoto <y.yama683@gmail.com>
2021-03-16 17:51:15 +0900

    test: add simple test

---
+++ tests/test.py
@@ -0,0 +1,25 @@
+from mastodon import Mastodon
+from tag_notifier.listener import Listener
+
+status = dict(
+    account=dict(id=0, acct="tester", bot=False),
+    content='<p>mastodon bot <br /><a href="https://example.com/tags/test" \
+        class="mention hashtag" rel="tag">#<span>test</span></a></p>',
+    tags=[dict(name="test1"), dict(name="test2")],
+    url="<url>",
+)
+
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+    import os
+
+    load_dotenv()
+
+    mastodon = Mastodon(
+        client_id=os.environ["client_id"],
+        client_secret=os.environ["client_secret"],
+        access_token=os.environ["access_token"],
+        api_base_url=os.environ["url"],
+    )
+    listener = Listener(mastodon, bot_id=os.environ["bot_id"], debug=True)
+    listener.on_update(status)
